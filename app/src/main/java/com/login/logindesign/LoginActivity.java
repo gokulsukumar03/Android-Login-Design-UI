a401bac306a08554d2f9c914f09e3cf7f5590884
Sukumar M <sukumar031@gmail.com>
2018-06-26 18:56:42 +0530

    new splash design added.

--- app/src/main/java/com/login/logindesign/LoginActivity.java
+++ app/src/main/java/com/login/logindesign/LoginActivity.java
@@ -1,13 +1,63 @@
 package com.login.logindesign;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private String tag = getClass().getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        startActivity(new Intent(this, SplashActivity.class));
+
+        /*MyThreadOne one = new MyThreadOne();
+        one.start();
+        MyThreadTwo two = new MyThreadTwo();
+        two.start();
+
+        HashMap h = new HashMap();
+        HashSet s = new HashSet();
+        Hashtable t = new Hashtable();*/
+
     }
+
+    private class MyThreadOne extends Thread{
+
+        @Override
+        public void run() {
+            super.run();
+            syncTest(5);
+        }
+    }
+
+    private class MyThreadTwo extends Thread {
+
+        @Override
+        public void run() {
+            super.run();
+            syncTest(100);
+        }
+    }
+
+    private synchronized void syncTest(int n){
+
+        for(int i=1;i<6;i++){
+
+            Log.d(tag, "   "   + i*n);
+            try {
+                Thread.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+    }
+
 }
---
+++ app/src/main/java/com/login/logindesign/SplashActivity.java
@@ -0,0 +1,30 @@
+package com.login.logindesign;
+
+import android.graphics.Typeface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.TextView;
+
+public class SplashActivity extends AppCompatActivity {
+
+
+    protected Typeface mTfRegular;
+    TextView app_name;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash);
+
+
+        app_name = (TextView) findViewById(R.id.app_name);
+        mTfRegular = Typeface.createFromAsset(getAssets(), "logo_regular.ttf");
+
+        app_name.setTypeface(mTfRegular);
+
+    }
+}
